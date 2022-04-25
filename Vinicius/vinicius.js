9c2c9d2bdaf1fe40382f7700ebe51f7d9eaae77a
Ramom Ferraz <ramomferraz@hotmail.com>
2022-04-24 23:15:57 -0300

    Adicionado botões de 'voltar' e 'jogar'

--- Vinicius/vinicius.js
+++ Vinicius/vinicius.js
@@ -1,4 +1,4 @@
-function clickVictim() {
+function clickVinicius() {
 
     alert("Após um evento que aconteceu em seu trabalho, Vinicius conseguiu alguns dias de folga e finalmente vai conseguir viajar para visitar sua família! Arrumou suas coisas e partiu rumo a sua viagem!")
     var rodoviaria = prompt("Pegar o atalho para rodiaviária?")
@@ -31,50 +31,3 @@ function clickVictim() {
 function back() {
     return location.href = "http://127.0.0.1:5500/Menu/index.html"
 }
-
-
-
-
-
-function clickKiller() {
-    var marcos = prompt("Marcos tem 45 anos, e possui um temperamento forte. Mora proximo a uma rodoviária. Sempre em seu tempo livre costuma receber os novos visitantes que chegam a sua pequena cidade. Deseja escolher esse personagem? Digite 'sim' para selecionar, ou 'não' para voltar a seleção de personagens.")
-    if (vinicius.toLowerCase() == "sim") {
-        alert("")
-        var briga = prompt("Deseja interferir na briga que está acontecendo?")
-
-        if (briga.toLowerCase() == "sim") {
-            alert("Briga com uma pessoa e acaba presa. FIM DE JOGO!")
-        }
-        else {
-            alert("Achou melhor não se meter e ficar observando a confusão de longe.")
-            var seguir = prompt("Seguir um dos participantes da briga?")
-
-            if (seguir.toLowerCase() == "sim") {
-                alert("Segue o alvo até o banheiro.")
-            }
-            else {
-                alert("Foi levado como testemunha pra delegacia.  FIM DE JOGO!")
-            }
-
-            var porta = prompt("TRANCOU A PORTA?")
-            if (porta.toLowerCase() == "sim") {
-                alert("Conseguiu matar a vitíma e fugir pela janela. PARABÉNS, VOCÊ VENCEU!!")
-            }
-            else {
-                alert("Embarca no ônibus e vai feliz ver sua familia. VOCÊ VENCEU!!!")
-            }
-        }
-
-    }
-}
-
-
-
-
-function clickCop() {
-    prompt("Brenda tem 36 anos, e ama caminhar. Durante suas caminhadas sempre que passa em frente a antiga rodoviária da cidade, entra para comer o seu sanduíche favorito. Deseja escolher esse personagem? Digite 'sim' para selecionar, ou 'não' para voltar a seleção de personagens.")
-
-
-}
-
-
